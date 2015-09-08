1051246ffa25416f711d024fa673091b9db6a9f4
nikila-saravanan <nikila.saravanan@gmail.com>
2015-09-08 18:04:37 -0400

    Done.

--- js/bike.js
+++ js/bike.js
@@ -1,7 +1,22 @@
-function Bike(){
+function Bike(name,type,color){
   // attributes go here...
+  this.name = name;
+  this.bikeType = type;
+  this.frameColor = color;
+  this.distance_ridden = 0;
+  this.condition = "ready to go!";
+  this.WHEELS = 2;
 }
 
-Bike.prototype.someInstanceMethod = function(){
+Bike.prototype.takeForARide = function(distance){
   // code goes here...
+  this.distance_ridden += distance
+  if (this.distance_ridden > 50) {
+    this.condition = "needs a tune up"
+  };
+};
+
+Bike.prototype.tuneUp = function(){
+  // code goes here...
+  this.distance_ridden = 0;
 };
--- js/owner.js
+++ js/owner.js
@@ -1,7 +1,20 @@
-function Owner(){
+function Owner(name){
   // attributes go here...
+  this.name = name;
+  this.bikes = [];
 }
 
-Owner.prototype.someInstanceMethod = function(){
+Owner.prototype.buildBike = function(name,type,color){
   // code goes here...
+  bike = new Bike(name,type,color);
+  this.bikes.push(bike);
+};
+
+Owner.prototype.rideBike = function(bike,distance){
+  // code goes here...
+  for (var i = 0; i < this.bikes.length; i++) {
+	   if (this.bikes[i].name == bike) {
+       this.bikes[i].takeForARide(distance);}
+  }
+
 };
